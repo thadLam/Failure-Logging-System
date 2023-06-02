0a438f29369efc8c608b42c9be4ec2a857181fe0
thaddaeusl <thaddaeusl@MNPWL-054>
2023-06-02 16:02:57 -0700

    navigation bar

---
+++ Failure Logging System/Failure Logging System/Controllers/RecordController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Failure_Logging_System.Controllers
+{
+    public class RecordController : Controller
+    {
+        // GET: RecordController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: RecordController/ViewRecord
+        public IActionResult ViewRecord()
+        {
+            return View();
+        }
+
+        // GET: RecordController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RecordController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RecordController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RecordController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RecordController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RecordController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RecordController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
