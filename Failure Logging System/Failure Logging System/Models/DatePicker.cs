3bbb51b904f50f08597f684d17402b2eb2fa45fd
jlsadmin <jlsadmin@DESKTOP-2I3IQCS>
2023-07-06 15:39:26 -0700

    working datepicker

---
+++ Failure Logging System/Failure Logging System/Models/DatePicker.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Failure_Logging_System.Models
+{
+    public class DatePicker
+    {
+        [Required]
+        [Display(Name = "RegisterDate")]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd hh:mm:ss}")]
+        public DateTime RegisterDate { get; set; }
+    }
+}
